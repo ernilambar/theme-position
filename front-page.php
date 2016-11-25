89e83eace6bd8f2b357c4583b4f60784ea453a98
ernilambar <nilambar@outlook.com>
2016-11-25 12:07:05 +0545

    Add basic styling

--- front-page.php
+++ front-page.php
@@ -12,7 +12,8 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<h3>here</h3>
+			<?php $theme_slug = 'university-hub'; ?>
+			<h4>Result for: <em><?php echo $theme_slug; ?></em></h4>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
--- functions.php
+++ functions.php
@@ -106,6 +106,8 @@ function theme_position_scripts() {
 
 	wp_enqueue_script( 'theme-position-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
+	wp_enqueue_script( 'theme-position-custom', get_template_directory_uri() . '/js/custom.js', array( 'jquery' ), '1.0.0', true );
+
 	wp_enqueue_script( 'theme-position-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
