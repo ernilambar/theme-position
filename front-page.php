5c216bed5be548c222a01d0185eea53e129444f0
ernilambar <nilambar@outlook.com>
2016-11-25 11:56:45 +0545

    Initial commit

---
+++ front-page.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Front page template file.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Theme_Position
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<h3>here</h3>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
